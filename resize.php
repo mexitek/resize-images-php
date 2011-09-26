8194fa470044aed9b0944c0164629329e834a849
Arlo Carreon <arlo.carreon@fhchs.edu>
2011-09-26 14:04:25 -0400

    Initial commit

---
+++ resize.php
@@ -0,0 +1,37 @@
+<?php 
+  // Check options at: http://www.white-hat-web-design.co.uk/blog/resizing-images-with-php/ 
+	include('SimpleImage.php');
+  
+	// Choose current folder
+	$handle = opendir(".");
+	
+	// Collection of extensions
+	$mimes = array('jpg','jpeg','png','gif');
+	
+	// Resized file pre
+	$prefix = '500x500_';
+	
+	// Scale Percentage
+	$scale = 50;
+	
+	// ------------------------------------------------------------------------------------------
+	// Read all the files in this folder
+	while( ($file = readdir($handle)) !== false )
+	{
+		$ext = substr($file, strpos($file,'.')+1, strlen($file) );
+		if( in_array( strtolower( $ext ), $mimes) &&  strpos($file,$prefix) === FALSE )
+		{
+			$image = new SimpleImage();
+			$image->load($file);
+			# chek options at: http://www.white-hat-web-design.co.uk/blog/resizing-images-with-php/
+			$image->scale($scale); 
+			$image->save($prefix.$file);
+			// print the change
+			echo $prefix.$file.', ';
+		}
+			
+	}
+	
+	
+	
+?>
